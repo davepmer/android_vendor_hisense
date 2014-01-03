307c83764c870c831587a2867709dc6902ba83fa
davepmer20102010@gmail.com <davepmer20102010@gmail.com>
2014-01-02 22:40:32 -0500

    fix link

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
     vendor/hisense/m470/proprietary/bin/brcm_patchram_plus:system/bin/brcm_patchram_plus \
     vendor/hisense/m470/proprietary/bin/btmacwriter:system/bin/btmacwriter \
     vendor/hisense/m470/proprietary/bin/dbus-daemon:system/bin/dbus-daemon \
-    vendor/hisense/m470/proprietary/bin/glgps:system/bin/glgps \
+    vendor/hisense/m470/proprietary/bin/glgps_nvidiaTegra2android:system/bin/glgps \
     vendor/hisense/m470/proprietary/bin/glgps_nvidiaTegra2android:system/bin/glgps_nvidiaTegra2android \
     vendor/hisense/m470/proprietary/bin/nvcpud:system/bin/nvcpud \
     vendor/hisense/m470/proprietary/bin/nv_hciattach:system/bin/nv_hciattach \
